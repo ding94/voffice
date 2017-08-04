92d1a284777df4f95942892e03662bf7bcf0a39c
soonws <soonws97@gmail.com>
2017-08-04 16:58:09 +0800

    create 10 new voucher

--- backend/views/vouchers/index.php
+++ backend/views/vouchers/index.php
@@ -15,7 +15,8 @@ use iutbay\yii2fontawesome\FontAwesome as FA;
     <?=Html::beginForm(['vouchers/batch'],'post');?>
 	<?= Html::a('Add New Voucher', ['/vouchers/add'], ['class'=>'btn btn-success']) ?>
     <?=Html::submitButton('Generate 10 new Vouchers', ['name'=>'gen', 'value' => 'gen','class' => 'btn btn-danger',]);?>
-    <?=Html::submitButton('Remove Vouchers', ['class' => 'btn btn-danger',]);?>
+
+    <?=Html::submitButton('Remove Vouchers', ['name'=>'remove', 'value' => 'remove','class' => 'btn btn-danger',]);?>
 	<?= GridView::widget([
         'dataProvider' => $model,
         'filterModel' => $searchModel,
