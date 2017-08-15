0afc2eacf5a10a4b36b7946e44cb7a0634eb72fc
darrensoap <darrensoap@gmail.com>
2017-08-15 17:11:48 +0800

    incomplete

--- common/config/main.php
+++ common/config/main.php
@@ -23,14 +23,23 @@ return [
             'timeZone' => 'Asia/Kuala_Lumpur',
         ],
     ],
-   /* 'modules' => [
-       'gridview' =>  [
-            'class' => '\kartik\grid\Module'
+    'modules' => [
+       // 'gridview' =>  [
+       //      'class' => '\kartik\grid\Module'
             // enter optional module parameters below - only if you need to  
             // use your own export download action or custom translation 
             // message source
             // 'downloadAction' => 'gridview/export/download',
             // 'i18n' => []
+        // ]
+        'notifications' => [
+            'class' => '\machour\yii2\notifications\NotificationsModule',
+            'notificationClass' => 'common\widgets\Notification',
+            'allowDuplicate' => false,
+            'dbDateFormat' => 'Y-m-d H:i:s',
+            'userId' => function() {
+                return \Yii::$app->user->id;
+            }
         ]
-    ],*/
+    ],
 ];
