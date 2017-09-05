582304550ef644b916f31a6abe4490906f1b5bed
ding94 <dw-ding@hotmail.co.uk>
2017-09-05 10:22:30 +0800

    add visible for each menu bar

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -29,75 +29,67 @@
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu'],
-                
+              
                 'items' => [
                     ['label' => 'Control Page', 'options' => ['class' => 'header']],
                     ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Function',
-                        'icon' => 'dashboard',
-                        'toggleData'=>true,
-                        'url' => '#',
-                        'items' => [
-                            [
-                                'label' => 'Admin Controller' , 'url' => '#', 'icon' => 'dashboard',
-                                'items' => [
+                    [   'label' => 'Admin Controller' , 'url' => '#', 'icon' => 'lock',
+                        'items' =>  [
                                         [ 'label' => 'Admin List', 'icon' => 'circle-o', 'url' => ['/admin/index']],
-                                     ],
-                                ],
-  
-                            [
-                                'label' => 'User Controller', 'icon' => 'dashboard', 'url' => "#",
-                                'items' => [
-                                   [ 'label' => 'User List', 'icon' => 'circle-o', 'url' => ['/user/index']],
-                                   [ 'label' => 'User Voucher', 'icon' => 'circle-o', 'url' => ['/user/uservoucherlist']],
-                                ]
-                            ],
-                            [
-                                'label' => 'Package Controller', 'icon' => 'dashboard', 'url' => '#',
-                                'items' => [
-                                    ['label' => 'Package List', 'icon' => 'circle-o', 'url' => ['/package/index']],
-                                    [ 'label' => 'Add Package', 'icon' => 'circle-o', 'url' => ['/package/add']],
-                                ],
-                            ],
-                            [
-                                'label' => 'Voucher Controller' , 'icon' => 'dashboard' ,'url' => '#',
-                                'items' => [
-                                    ['label' => 'Vouchers List' ,'icon' => 'circle-o' , 'url' => ['/vouchers/index'],
                                     ],
-                                ],
-                            ],
-							[
-                                'label' => 'Finance Controller', 'icon' => 'dashboard', 'url' => '#',
-                                'items' => [
-                                    ['label' => 'Offline Topup', 'icon' => 'circle-o', 'url' => ['/finance/topup/index']],
-									['label' => 'User Withdraw', 'icon' => 'circle-o', 'url' => ['/finance/withdraw/index']],
-                                    ],
-                            ],
-                            [
-                                'label' => 'Logistic Controller', 'icon' => 'dashboard', 'url' => '#',
-                                'items' => [
-                                    ['label' => 'Add Mail', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/received-mail']],
-                                    ['label' => 'Received Mail', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>1]],
-                                    ['label' => 'Pending Pick Up', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>2]],
-                                    ['label' => 'Sending Mail', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>3]],
-                                    ['label' => 'Confirm received', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>4]],
-                                    ['label' => 'Early postal', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>5]],
-                                    ['label' => 'Pending early pickup', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>6]],
-                                ],
-                            ],
-                            [
-                                'label' => 'Auth Controller' , 'icon' => 'gamepad' ,'url' => '#',
-                                'items' => [
-                                    ['label' => 'Auth List' ,'icon' => 'circle-o' , 'url' => ['/auth/index']],
-                                    ['label' => 'Permission List' ,'icon' => 'circle-o' , 'url' => ['/auth/permission']],
-                                ],
-                            ],
-                            
-                        ],
+                        'visible'=> Yii::$app->user->can('admin/index'), 
                     ],
+                    [   'label' => 'User Controller', 'icon' => 'user', 'url' => "#",
+                        'items' =>  [
+                                        [ 'label' => 'User List', 'icon' => 'circle-o', 'url' => ['/user/index']],
+                                        [ 'label' => 'User Voucher', 'icon' => 'circle-o', 'url' => ['/user/uservoucherlist']],
+                                    ],
+                        'visible'=> Yii::$app->user->can('user/index'), 
+                    ],
+                    [   'label' => 'Package Controller', 'icon' => 'dashboard', 'url' => '#',
+                        'items' =>  [
+                                        ['label' => 'Package List', 'icon' => 'circle-o', 'url' => ['/package/index']],
+                                        [ 'label' => 'Add Package', 'icon' => 'circle-o', 'url' => ['/package/add']],
+                                    ],
+                        'visible'=> Yii::$app->user->can('package/index'), 
+                    ],
+                    [   'label' => 'Voucher Controller' , 'icon' => 'dashboard' ,'url' => '#',
+                        'items' =>  [
+                                        ['label' => 'Vouchers List' ,'icon' => 'circle-o' , 'url' => ['/vouchers/index']],
+                                    ],
+                        'visible'=> Yii::$app->user->can('vouchers/index'), 
+                    
+                    ],
+					[   'label' => 'Finance Controller', 'icon' => 'money', 'url' => '#',
+                        'items' =>  [
+                                        ['label' => 'Offline Topup', 'icon' => 'circle-o', 'url' => ['/finance/topup/index']],
+									    ['label' => 'User Withdraw', 'icon' => 'circle-o', 'url' => ['/finance/withdraw/index']],
+                                    ],
+                        'visible'=> Yii::$app->user->can('topup/index'), 
+                    ],
+                    [   'label' => 'Logistic Controller', 'icon' => 'dashboard', 'url' => '#',
+                        'items' =>  [
+                                        ['label' => 'Add Mail', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/received-mail']],
+                                        ['label' => 'Received Mail', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>1]],
+                                        ['label' => 'Pending Pick Up', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>2]],
+                                        ['label' => 'Sending Mail', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>3]],
+                                        ['label' => 'Confirm received', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>4]],
+                                        ['label' => 'Early postal', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>5]],
+                                        ['label' => 'Pending early pickup', 'icon' => 'circle-o', 'url' => ['/logistics/parcel/type-mail' ,'status'=>6]],
+                                    ],
+                        'visible'=> Yii::$app->user->can('parcel/type-mail'), 
+                    ],
+                    [   'label' => 'Auth Controller' , 'icon' => 'cog' ,'url' => '#',
+                        'items' =>  [
+                                        ['label' => 'Auth List' ,'icon' => 'circle-o' , 'url' => ['/auth/index']],
+                                        ['label' => 'Permission List' ,'icon' => 'circle-o' , 'url' => ['/auth/permission']],
+                                    ],
+                    ],
+                            
+                        
                 ],
+                
             ]
         ) ?>
 
