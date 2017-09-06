f87fd81da74f877b89b8648ce2c68cdd4720ec7c
darrensoap <darrensoap@gmail.com>
2017-09-06 16:17:26 +0800

    subscribe incomplete

--- common/models/Payment.php
+++ common/models/Payment.php
@@ -7,12 +7,15 @@ use Yii;
 /**
  * This is the model class for table "payment".
  *
- * @property integer $pay_id
+ * @property integer $id
  * @property integer $uid
- * @property string $addTime
- * @property double $amount
- * @property integer $package_type
- * @property integer $type
+ * @property string $paid_amount
+ * @property integer $paid_type
+ * @property integer $bank_acc
+ * @property string $item
+ * @property string $original_price
+ * @property integer $voucher_id
+ * @property integer $discount
  */
 class Payment extends \yii\db\ActiveRecord
 {
@@ -30,10 +33,10 @@ class Payment extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['uid', 'amount', 'package_type', 'type'], 'required'],
-            [['uid', 'package_type', 'type',], 'integer'],
-            [['addTime'],'safe'],
-            [['amount'], 'number'],
+            [['uid', 'paid_amount', 'paid_type', 'item', 'original_price'], 'required'],
+            [['uid', 'paid_type', 'bank_acc', 'voucher_id', 'discount'], 'integer'],
+            [['paid_amount', 'original_price'], 'number'],
+            [['item'], 'string'],
         ];
     }
 
@@ -43,12 +46,15 @@ class Payment extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'pay_id' => 'Pay ID',
+            'id' => 'ID',
             'uid' => 'Uid',
-            'addTime' => 'Add Time',
-            'amount' => 'Amount',
-            'package_type' => 'Package Type',
-            'type' => 'Type',
+            'paid_amount' => 'Paid Amount',
+            'paid_type' => 'Paid Type',
+            'bank_acc' => 'Bank Acc',
+            'item' => 'Item',
+            'original_price' => 'Original Price',
+            'voucher_id' => 'Voucher ID',
+            'discount' => 'Discount',
         ];
     }
 }
