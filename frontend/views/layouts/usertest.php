195d1e479579ddf09930b20dd1334d76ea7cfb73
darrensoap <darrensoap@gmail.com>
2017-09-13 11:44:27 +0800

    mobile user profile ui fix

--- frontend/views/layouts/usertest.php
+++ frontend/views/layouts/usertest.php
@@ -71,24 +71,20 @@ AppAsset::register($this);
     ?>
 
 		
-		<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand page-scroll" href="<?php echo yii\helpers\Url::to(['site/index'])?>">X MailBox<span><?php //echo $this->params['parcel']; ?></span></a>
+                <a class="navbar-brand" href="<?php echo yii\helpers\Url::to(['site/index'])?>">X MailBox<span><?php //echo $this->params['parcel']; ?></span></a>
+                <a class="navbar-toggle" href="<?php echo yii\helpers\Url::to(['site/logout'])?>" data-method="post">
+                    Logout
+                </a>
             </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
+            <ul class="nav navbar-nav navbar-right hidden-xs">
                     <li>
                         <a class="page-scroll" href="<?php echo yii\helpers\Url::to(['site/logout'])?>" data-method="post">Logout</a>
                     </li>
-                </ul>
-            </div>
+            </ul>
             <!-- /.navbar-collapse -->           
         </div>
         <!-- /.container-fluid -->
