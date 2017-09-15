64ebd8ffef6941f60eb1e80a84564958d0dfcf8d
darrensoap <darrensoap@gmail.com>
2017-09-15 17:29:21 +0800

    send receipt to email after successful subscription complete excluding html styling

--- common/mail/SubscriptionReceipt-html.php
+++ common/mail/SubscriptionReceipt-html.php
@@ -9,31 +9,90 @@ use yii\helpers\Url;
     <h4>Subscription Successful!</h4>
 </div>
 
-<div class="container">
-	<div class="tab-content col-md-6 col-md-offset-1" id="userprofile">
-		<table class="table table-user-information"><h1>User Package</h1>
-                <tbody>
-                  <tr>
-                    <td>My Package:</td>
-                    <td><?php echo $model['package']['type']; ?></td>
-				</tr>
-				<tr>
-                    <td>Package Price (RM):</td>
-					 <td><?php echo $model['package']['price']; ?></td>
-				</tr>
-				<tr>
-                    <td>Subscribe Period (days):</td>
-				<td><?php echo $model['sub_period']; ?></td>
-					</tr>
-					<tr>
-                    <td>Subscription Starts On:</td>
-				<td><?php echo $model['subscribe_time']; ?></td>
-					</tr>
-				<tr>
-                    <td>Subscription Ends On:</td>
-					<td><?php echo $model['end_period']; ?></td>
-				</tr>
-               </tbody>
-            </table>		
-	</div>
+<div class="invoice-box">
+                <div class="invoice-box">
+        <table cellpadding="0" cellspacing="0">
+            <tr class="top">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td>
+                                Created: January 1, 2015<br>
+                                <span>Next Payment:</span><?php echo $userpackagesubscription['next_payment']; ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            
+            <tr class="information">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td>
+                                <span>User Name:</span><?php echo $user['username']; ?><br>
+                                <span>Name:</span><?php echo $userdetails['Fname']; ?><?php echo $userdetails['Lname']; ?><br>
+                            </td>
+                            
+                            <td>
+                                <span>Address:</span><?php echo $userdetails['address1']; ?><br>
+                                <?php echo $userdetails['address2']; ?><br>
+                                <?php echo $userdetails['address3']; ?><br>
+                                <span>Postcode:</span><?php echo $userdetails['postcode']; ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            
+            <tr class="heading">
+                <td>
+                    Payment Method
+                </td>
+                
+                <td>
+                    Check #
+                </td>
+            </tr>
+            
+            <tr class="details">
+                <td>
+                    Check
+                </td>
+                
+                <td>
+                    1000
+                </td>
+            </tr>
+            
+            <tr class="heading">
+                <td>
+                    Item
+                </td>
+                
+                <td>
+                    Price(RM)
+                </td>
+            </tr>
+            
+            <tr class="item">
+                <td>
+                    <span>Subscription:</span><?php echo $model['package']['type']; ?><br>
+                    <span>Subscription Start:</span><?php echo $model['subscribe_time']; ?><br>
+                    <span>Subscription End:</span><?php echo $model['end_period']; ?>
+                </td>
+                
+                <td>
+                    <span>RM</span><?php echo $model['package']['price']; ?>
+                </td>
+            </tr>
+            
+            <tr class="total">
+                <td></td>
+                
+                <td>
+                   Total: $385.00
+                </td>
+            </tr>
+        </table>	
 </div>
