0a65fb812121a469f237e6866fe121b9efa2bf96
ding94 <dw-ding@hotmail.co.uk>
2017-09-15 16:57:46 +0800

    remove useless thing

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -113,23 +113,6 @@ use common\models\Notification\Notification;
                         <li>
                             <!-- inner menu: contains the actual data -->
                             <ul class="menu">
-                                <!-- <li>
-                                    <a href="#">
-                                        <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-warning text-yellow"></i> Very long description here that may
-                                        not fit into the page and may cause design problems
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-users text-red"></i> 5 new members joined
-                                    </a>
-                                </li>
-                                 -->
                                 <?php foreach($notic as $data):?>
                                 <li>
                                     <a href="#">
@@ -137,11 +120,6 @@ use common\models\Notification\Notification;
                                     </a>
                                 </li>
                                 <?php endforeach ;?>
-                                <!-- <li>
-                                    <a href="#">
-                                        <i class="fa fa-user text-red"></i> You changed your username
-                                    </a>
-                                </li> -->
                             </ul>
                         </li>
                         <li class="footer"><a href="#">View all</a></li>
