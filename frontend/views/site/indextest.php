d0ea16e59bbe3b4ca21d6e76bface5dcc8f9e7be
ding94 <dw-ding@hotmail.co.uk>
2018-03-30 14:29:54 +0800

    nothing

--- frontend/views/site/indextest.php
+++ frontend/views/site/indextest.php
@@ -22,6 +22,7 @@ $this->title = 'Virtual Office';
             ],]); ?>
         <?= Yii::$app->session->getFlash('success');?>
     <!-- Header -->
+
     <header style="padding-top: 50px">
         <div class="header-container">
             <div class="header-img">
@@ -235,3 +236,19 @@ $this->title = 'Virtual Office';
 	   </div>
 	<?php ActiveForm::end();?>
     </section>
+
+    <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ab85ab44b401e45400e09ea/1c9g2v8qt';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<!--End of Tawk.to Script-->
